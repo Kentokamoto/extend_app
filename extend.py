d16742c533006ef6036df64ba892519b1ad398b6
Kento Okamoto <kentokamoto@protonmail.com>
2023-02-17 21:39:24 -0800

    Transaction List

--- extend.py
+++ extend.py
@@ -1,7 +1,10 @@
 import cmd
+import datetime
 import getpass
+from prettytable import PrettyTable
 
 from models.account import Account
+from models.cards import Cards
 from util.api import ExtendAPI
 
 
@@ -10,51 +13,92 @@ class ExtendShell(cmd.Cmd):
     prompt = "(Extend CLI) >"
     file = None
     extend_account: Account = None
+    cards: Cards = None
     api = ExtendAPI()
 
     def do_test(self, arg) -> None:
-        'Test Interpreter'
+        "Test Interpreter"
         print("My arg is {}".format(arg))
 
     def do_login(self, arg) -> None:
-        'Sign into Extend account'
+        "Sign into Extend account"
         email = input("email: ")
         password = getpass.getpass()
-        self.extend_account = self.api.signin(email=email,
-                                              password=password)
+        self.extend_account = self.api.signin(email=email, password=password)
         print(self.extend_account.dict())
 
     def do_user(self, arg) -> None:
-        'Get information about a user'
+        "Get information about a user"
         if not self.extend_account:
             print("Not signed in. Type login to sign into an existing Extend account")
         else:
             id = arg if arg else "me"
-            user = self.api.get_user(id=id,
-                                        bearer_token=self.extend_account.token)
+            user = self.api.get_user(id=id, bearer_token=self.extend_account.token)
             print(user.dict())
 
     def do_cards(self, arg) -> None:
-        'List all virtual cards available to your user, including the available balance remaining'
+        "List all virtual cards available to your user, including the available balance remaining"
         if not self.extend_account:
             print("Not signed in. Type login to sign into an existing Extend account")
         else:
             id = arg if arg else "me"
-            cards = self.api.get_cards(bearer_token=self.extend_account.token)
-            print(cards.dict())
+            params = {}
+            self.cards = self.api.get_cards(
+                bearer_token=self.extend_account.token, query_params=params
+            )
+            print(self.cards.dict())
 
     # TODO
     def do_transactions(self, arg) -> None:
-        'List the transactions associated with your virtual card.'
-        pass
+        "List the transactions associated with your virtual card."
+        if not self.extend_account:
+            print("Not signed in. Type login to sign into an existing Extend account")
+            return
+        if not self.cards:
+            self.cards = self.api.get_cards(
+                bearer_token=self.extend_account.token, query_params={}
+            )
+
+        card_number = 0
+        if not arg:
+            table = PrettyTable(
+                ["Card Number", "Card ID", "Name", "Last 4", "Exp Date"]
+            )
+            for index, card in enumerate(self.cards.virtualCards):
+                card_id = card.id
+                name = card.displayName
+                last4 = card.last4
+                exp_date = datetime.datetime.strptime(
+                    card.expires, "%Y-%m-%dT%H:%M:%S.%f%z"
+                ).strftime("%m/%Y")
+                table.add_row([index, card_id, name, last4, exp_date])
+            print(table)
+
+            card_number = int(input("Select card number:"))
+        else:
+            if int(arg) > len(self.cards.virtualCards):
+                print(
+                    "Invalid card number. Card number must be between 0 and {}".format(
+                        len(self.cards.virtualCards)
+                    )
+                )
+                return
+
+            card_number = int(arg)
+
+        card_id = self.cards.virtualCards[card_number].id
+        transaction_list = self.api.get_transaction_list(
+            bearer_token=self.extend_account.token, card_id=card_id
+        )
+        print(transaction_list.dict())
 
     # TODO
     def do_transaction_detail(self, arg) -> None:
-        'View the details for each individual transaction you’ve made.'
+        "View the details for each individual transaction you’ve made."
         pass
 
     def do_EOF(self, line) -> bool:
-        'Logout of extend account and close shell'
+        "Logout of extend account and close shell"
         print()
         print("Logging out")
         # Logout script
@@ -62,5 +106,6 @@ class ExtendShell(cmd.Cmd):
             self.api.signout(self.extend_account.token)
         return True
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     ExtendShell().cmdloop()
--- util/api.py
+++ util/api.py
@@ -1,22 +1,19 @@
 import json
 import requests
-from models import account, cards
+from models import account, cards, transactions
 
 
-class ExtendAPI():
+class ExtendAPI:
     def __init__(self):
         self.api_endpoint = "https://api.paywithextend.com/"
         self.header = {
             "content-type": "application/json",
-            "Accept": "application/vnd.paywithextend.v2021-03-12+json"
-            }
+            "Accept": "application/vnd.paywithextend.v2021-03-12+json",
+        }
 
     def signin(self, email: str, password: str) -> account.Account:
         url = self.api_endpoint + "signin"
-        body = {
-                "email": email,
-                "password": password
-                }
+        body = {"email": email, "password": password}
         try:
             res = requests.post(url, headers=self.header, json=body)
             return account.Account.parse_raw(res.text)
@@ -26,10 +23,10 @@ class ExtendAPI():
             print("Failed to signin: {}".format(e))
 
     def get_user(self, bearer_token: str, id="me") -> account.User:
-        '''
+        """
         Get user information:
         "me" refers to currently authenticated user
-        '''
+        """
         url = self.api_endpoint + "users/{id}".format(id=id)
         header = self.header.copy()
         header["Authorization"] = "Bearer {}".format(bearer_token)
@@ -43,7 +40,7 @@ class ExtendAPI():
         except Exception as e:
             print("Failed to get account: {}".format(e))
 
-    def get_cards(self, bearer_token, query_params={}) -> cards.Cards:
+    def get_cards(self, bearer_token, query_params) -> cards.Cards:
         # TODO Handle for query parameters
         url = self.api_endpoint + "virtualcards"
 
@@ -56,7 +53,21 @@ class ExtendAPI():
             print("Failed to create account: {}".format(ve))
         except Exception as e:
             print("Failed to get account: {}".format(e))
-        
+
+    def get_transaction_list(
+        self, bearer_token, card_id
+    ) -> transactions.TransactionList:
+        url = self.api_endpoint + "virtualcards/" + card_id + "/transactions"
+
+        header = self.header.copy()
+        header["Authorization"] = "Bearer {}".format(bearer_token)
+        try:
+            res = requests.get(url, headers=header)
+            return transactions.TransactionList.parse_raw(res.text)
+        except ValidationError as ve:
+            print("Failed to create account: {}".format(ve))
+        except Exception as e:
+            print("Failed to get account: {}".format(e))
 
     def signout(self, bearer_token: str):
         url = self.api_endpoint + "signout"
@@ -65,7 +76,7 @@ class ExtendAPI():
         try:
             res = requests.delete(url, headers=header)
 
-            if 'msg' not in json.loads(res.text):
+            if "msg" not in json.loads(res.text):
                 raise Exception("Failed to signout")
         except Exception as e:
             print("Failed to signin: {}".format(e))
